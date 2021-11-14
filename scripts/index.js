18c7473d7f0dc5e6946a8b86f5653b93256e59a7
BJuice <beetle_juice84@mail.ru>
2021-11-14 03:11:57 +0300

    Исправить ошибки в JS и по БЭМ

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,7 @@ editButton.addEventListener('click', popupOpen);
 
 closeButton.addEventListener('click', popupClose);
 
-saveButton.addEventListener('click', popupSave);
+saveButton.addEventListener('click', formSubmitHandler);
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__input-form');
@@ -21,39 +21,26 @@ let descriptionInput = document.querySelector('[name="profile-description"]');
 let infoProfile = document.querySelector('.profile__info');
 let descriptionProfile = document.querySelector('.profile__description');
 
-let infoProfileCurrentValue = infoInput.value;
-let descriptionProfileCurrentValue = descriptionInput.value;
-
 function popupOpen() {
   popupElement.classList.add('popup__opened');
-  infoInput.textContent = infoProfileCurrentValue; //Запись в форму значений из профиля
-  descriptionInput.textContent = descriptionProfileCurrentValue; //Запись в форму значений из профиля  
-  console.log(infoInput.textContent)
-  console.log(descriptionInput.textContent)
+  infoInput.value = infoProfile.textContent; //Запись в форму значений из профиля
+  descriptionInput.value = descriptionProfile.textContent; //Запись в форму значений из профиля  
 }
 
 function popupClose() {
   popupElement.classList.remove('popup__opened');
-  infoInput.value = infoProfileCurrentValue;
-  descriptionInput.value = descriptionProfileCurrentValue;
-
-  console.log(infoInput.value)
-  console.log(descriptionInput.value)
 }
 
 function popupSave() {
   infoProfile.textContent = infoInput.value; //Запись в Профиль введенных значений из формы
   descriptionProfile.textContent = descriptionInput.value; //Запись в Профиль введенных значений из формы
-  infoProfileCurrentValue = infoInput.value;
-  descriptionProfileCurrentValue = descriptionInput.value;
-  popupClose()
+  popupClose();
 }
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-
+  popupSave();
 }
-
 
 formElement.addEventListener('submit', formSubmitHandler);
 
