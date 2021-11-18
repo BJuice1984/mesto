4140aaf3370a917ad8a3f789e58c8dd511eba12d
BJuice <beetle_juice84@mail.ru>
2021-11-18 04:12:42 +0300

    Исправить ошибки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,38 @@
-const popupElement = document.querySelector('.popup');
-
-const closeButton = document.getElementById('CloseButton');
-
-const editButton = document.getElementById('EditButton');
-
-const saveButton = document.getElementById('SaveButton');
-
-editButton.addEventListener('click', popupOpen);
-
-closeButton.addEventListener('click', popupClose);
-
-saveButton.addEventListener('click', formSubmitHandler);
-
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__input-form');
 // Находим поля формы в DOM
-let infoInput = document.querySelector('[name="profile-info"]');
-let descriptionInput = document.querySelector('[name="profile-description"]');
+let infoInput = document.querySelector('.popup__input-text_type_name');
+let descriptionInput = document.querySelector('.popup__input-text_type_description');
 // Находим поля профиля в DOM
 let infoProfile = document.querySelector('.profile__info');
 let descriptionProfile = document.querySelector('.profile__description');
 
-function popupOpen() {
-  popupElement.classList.add('popup__opened');
+const popupElement = document.querySelector('.popup');
+
+const closeButton = document.querySelector('.popup__close-button');
+
+const editButton = document.querySelector('.button_type_edit');
+
+function openPopup() {
+  popupElement.classList.add('popup_opened');
   infoInput.value = infoProfile.textContent; //Запись в форму значений из профиля
   descriptionInput.value = descriptionProfile.textContent; //Запись в форму значений из профиля
 }
 
-function popupClose() {
-  popupElement.classList.remove('popup__opened');
-}
-
-function popupSave() {
-  infoProfile.textContent = infoInput.value; //Запись в Профиль введенных значений из формы
-  descriptionProfile.textContent = descriptionInput.value; //Запись в Профиль введенных значений из формы
-  popupClose();
+function closePopup() {
+  popupElement.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  popupSave();
+  infoProfile.textContent = infoInput.value; //Запись в Профиль введенных значений из формы
+  descriptionProfile.textContent = descriptionInput.value; //Запись в Профиль введенных значений из формы
+  closePopup();
 }
+
+editButton.addEventListener('click', openPopup);
+
+closeButton.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
 
