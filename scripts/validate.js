e99c6ca65c110218fb56a269a93b556e0194976e
BJuice <beetle_juice84@mail.ru>
2021-12-05 03:34:21 +0300

    Валидация рабочая версия

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,3 +1,48 @@
+const setEventListeners = () => {
+  const formElement = document.querySelector('.popup__input-form');
+  formElement.addEventListener('submit', evt => evt.preventDefault());
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input-text'));
+  const buttonElement = formElement.querySelector('.popup__save-button');
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(formElement, buttonElement);
+    })
+  })
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input-form_type_error');
+  errorElement.classList.remove('popup__input-form-error_active');
+  errorElement.textContent = '';
+};
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+inputElement.classList.add('popup__input-form_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-form-error_active');
+};
+
+const toggleButtonState = (formElement, buttonElement) => {
+  const isFormValid = formElement.checkValidity();
+    buttonElement.classList.toggle('popup__save-button_disabled', !isFormValid);
+    buttonElement.disabled = !isFormValid;
+};
+
+setEventListeners();
+
+
+
 // const showInputError = (formElement, inputElement, errorMessage) => {
 // const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
 // inputElement.classList.add('popup__input-form_type_error');
@@ -31,16 +76,16 @@
 //   });
 // };
 
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.popup__input-form'));
-  console.log(formList);
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', function (evt) {
-      evt.preventDefault();
-    });
-    setEventListeners();
-  });
-};
+// const enableValidation = () => {
+//   const formList = Array.from(document.querySelectorAll('.popup__input-form'));
+//   console.log(formList);
+//   formList.forEach((formElement) => {
+//     formElement.addEventListener('submit', function (evt) {
+//       evt.preventDefault();
+//     });
+//     setEventListeners();
+//   });
+// };
 
 // const hasInvalidInput = (inputList) => {
 //   // проходим по этому массиву методом some
@@ -62,6 +107,6 @@ const enableValidation = () => {
 //   }
 // };
 
-enableValidation();
+// enableValidation();
 
 
